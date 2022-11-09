5e361c655f66501dd3f48318d463563cd9eacbcd
RUIPENG <ruipeng4@illinois.edu>
2022-11-08 21:21:42 -0600

    update database.py

--- flask/app/database.py
+++ flask/app/database.py
@@ -17,6 +17,118 @@ def login(NetId, Password):
     conn.close()
     return ret
 
+def show_schedule(netId, semester):
+    '''
+    Search for course schedule given netId and semester.
+    Input:
+    -- netId (string): NetId of a student
+    -- semester (string): given semester (e.g. FA22 or SP23)
+    Output:
+    -- schedule (list): each item of schedule is a tuple including course information
+        CRN, CourseId, Title, LectureType, LectureTime, Location, Credit, Grade
+        e.g. [
+            [00001, 'CS999', 'Some Title', 'Lecture-Discussion', 'MON09:00am-10:15am', 'CIF 3039', 3, 'A'],
+            [12345, 'CS888', 'Another Title', 'Online Lectures', '', '', 4, 'B']
+        ]
+    '''
+    raise NotImplementedError
+    return []
+
+def drop(netId, CRN):
+    '''
+    Drop a given course.
+    PS: You should check eligibility before dropping a course, including:
+        1. If the CRN is not enrolled by the student, return an error;
+        2. If a Grade is already granted for the course, prevent drop and return an error;
+    WARNING: You should always be cautious when performing DELETE queries!
+    Input: 
+    -- netId (string): NetId of a student
+    -- CRN (int): CRN of a section
+    Operation:
+        1. Check if the course can be dropped.
+        2. If the course can be dropped, delete it from Enrollment, return 0 (success)
+        3. Else, prevent the drop and return an error code (e.g. 1 for course not found, 2 for invalid drop, ... you can define for yourself)
+    Output:
+    -- return value (int): indicating dropping successful or not.
+    '''
+    raise NotImplementedError
+    return -1
+
+def change_credit(netId, CRN, credit):
+    '''
+    Change credit for a given course.
+    PS: You should check eligibility before update, including:
+        1. If the CRN is not enrolled by the student, return an error;
+        2. If a Grade is already granted for the course, prevent update and return an error;
+        3. If the credit is not avaliable for the course, prevent update and return an error;
+    Input: 
+    -- netId (string): NetId of a student
+    -- CRN (int): CRN of a section
+    -- credit (int): credit to be changed.
+    Operation:
+        1. Check if the course credit can be updated.
+        2. If can, make update, return 0 (success)
+        3. Else, prevent the update and return an error code (e.g. 1 for course not found, 2 for invalid update, ... you can define for yourself)
+    Output:
+    -- return value (int): indicating update successful or not.
+    '''
+    raise NotImplementedError
+    return -1
+
+def keyword_course_search(keyword):
+    '''
+    Given a keyword, search all relavant course information and return.
+        (Title contains keyword)
+    Input:
+    -- keyword (string): 
+    Output:
+    -- course_lst (list): a list, each item is a tuple of **course** information including
+        CourseId, Title, Department, Number of Sections, Enrolled Student number, Capacity
+        e.g. [
+            ['CS999', 'Some Title', 'CS', 3, 48, 50],
+            ['CS888', 'Another Title', 'CS', 4, 100, 100]
+        ]
+    PS: You may need to use GROUP BY and aggregation functions for this operation.
+    '''
+    raise NotImplementedError
+    return []
+
+def show_sections(courseId):
+    '''
+    Show all sections given a course id.
+    Input:
+    -- CourseId (string):
+    Output:
+    -- section_lst (list): a list, each item is a tuple of **section** information including
+        CRN, Lecture Type, Lecture Time, Location, Avaliable Credits(list), Instructor Names(list), Enrolled Student Number, Capacity, Restrictions
+        e.g. [
+            [00001, 'Lecture-Discussion', 'MON09:00am-10:15am', 'CIF 3039', [3, 4], ['Anna John', 'Bob Keith'], 10, 15, '']
+        ]
+    PS: You may use several SEARCH queries and process the data
+    '''
+    raise NotImplementedError
+    return []
+
+def enroll(netId, CRN):
+    '''
+    Enroll in Sections.
+    PS: You should check eligibility before inserting data into database, including:
+    - 1. Check restrictions and Level of student
+    - 2. Check Capacity of the section
+    - 3. Check if the students enrolled in the course before (have records of enrollment of the same CourseId)
+    Input
+    - netId (string)
+    - CRN (int)
+    Output
+    -- return value (int): indicating insert successful or not.
+    '''
+    raise NotImplementedError
+    return -1
+
+def student_search(condition):
+    raise NotImplementedError
+    return []
+
 '''
 def fetch_todo() -> dict:
     """Reads all tasks listed in the todo table
